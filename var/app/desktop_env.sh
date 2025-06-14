5292f92a3c33e3f5fce3be6a0faa6524295cf36d
alphaaurigae <4327142+alphaaurigae@users.noreply.github.com>
2025-06-14 17:59:18 +0200

    tidy

---
+++ var/app/desktop_env.sh
@@ -0,0 +1,56 @@
+#  BUDGIE - https://wiki.gentoo.org/wiki/Budgie
+BUDGIE_DSTENV_XEC="budgie_dpmexec"
+BUDGIE_DSTENV_STARTX="budgie"
+BUDGIE_DSTENV_EMERGE="budgie"
+#  CINNAMON - https://wiki.gentoo.org/wiki/Cinnamon
+CINNAMON_DSTENV_XEC="gnome-session-cinnamon"
+CINNAMON_DSTENV_STARTX="cinnamon-session"
+CINNAMON_DSTENV_EMERGE="gnome-extra/cinnamon"
+#  DDE "Deepin Desktop Environment" - https://wiki.gentoo.org/wiki/DDE
+DDE_DSTENV_XEC="DDE"
+DDE_DSTENV_STARTX="DDE"
+DDE_DSTENV_EMERGE="DDE"
+#  FVWM-Crystal - FVWM-Crystal
+FVWMCRYSTAL_DSTENV_XEC="fvwm-crystal"
+FVWMCRYSTAL_DSTENV_STARTX="fvwm-crystal"
+FVWMCRYSTAL_DSTENV_EMERGE="x11-themes/fvwm-crystal"
+#  GNOME - https://wiki.gentoo.org/wiki/GNOME
+GNOME_DSTENV_XEC="gnome-session"
+GNOME_DSTENV_STARTX="GNOME"
+GNOME_DSTENV_EMERGE="gnome-base/gnome"
+#  KDE - FVWM-Crystal
+KDE_DSTENV_XEC="kde-plasma/startkde"
+KDE_DSTENV_STARTX="startkde"
+KDE_DSTENV_EMERGE="kde-plasma/plasma-meta"
+#  LXDE - https://wiki.gentoo.org/wiki/LXDE
+LXDE_DSTENV_XEC="startlxde"
+LXDE_DSTENV_STARTX="startlxde"
+LXDE_DSTENV_EMERGE="lxde-base/lxde-meta"
+#  LXQT - FVWM-Crystal
+LXQT_DSTENV_XEC="startlxqt"
+LXQT_DSTENV_STARTX="startlxqt"
+LXQT_DSTENV_EMERGE="lxqt-base/lxqt-meta"
+#  LUMINA - https://wiki.gentoo.org/wiki/Lumina
+LUMINA_DSTENV_XEC="start-lumina-desktop"
+LUMINA_DSTENV_STARTX="start-lumina-desktop"
+LUMINA_DSTENV_EMERGE="x11-wm/lumina"
+#  MATE - https://wiki.gentoo.org/wiki/MATE
+MATE_DSTENV_XEC="mate-session"
+MATE_DSTENV_STARTX="mate-session"
+MATE_DSTENV_EMERGE="mate-base/mate"
+#  PANTHEON - https://wiki.gentoo.org/wiki/Pantheon
+PANTHEON_DSTENV_XEC="PANTHEON"
+PANTHEON_DSTENV_STARTX="PANTHEON"
+PANTHEON_DSTENV_EMERGE="PANTHEON"
+#  RAZORQT - FVWM-Crystal
+RAZORQT_DSTENV_XEC="razor-session"
+RAZORQT_DSTENV_STARTX="razor-session"
+RAZORQT_DSTENV_EMERGE="RAZORQT"
+#  TDE - https://wiki.gentoo.org/wiki/Trinity_Desktop_Environment
+TDE_DSTENV_XEC="tde-session"
+TDE_DSTENV_STARTX="tde-session"
+TDE_DSTENV_EMERGE="trinity-base/tdebase-meta"
+#  XFCE - https://wiki.gentoo.org/wiki/Xfce
+XFCE_DSTENV_XEC="xfce4-session"
+XFCE_DSTENV_STARTX="startxfce4"
+XFCE_DSTENV_EMERGE="xfce-base/xfce4-meta"
